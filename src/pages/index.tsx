1c571efec67ddb579c7d83316439484899631696
Ben Jenkins <ben@benjenkins.dev>
2023-02-20 16:25:12 -0600

    fix: resolve lint errors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,11 +5,11 @@ export default function Home() {
   return (
     <>
       <p className="text-gray-800 dark:text-gray-200 text-xl">
-        Hey, I'm a Fullstack Software Engineer at Screencastify working on video learning tools.  I enjoy solving exciting problems and working with a mission-minded team.
+        Hey, I&apos;m a Fullstack Software Engineer at Screencastify working on video learning tools.  I enjoy solving exciting problems and working with a mission-minded team.
       </p>
       <br />
       <p className="text-gray-800 dark:text-gray-200 text-xl">
-        Interested in working together?  Reach out and let's chat!
+        Interested in working together?  Reach out and let&apos;s chat!
       </p>
 
     </>
