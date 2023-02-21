9444343f1ea41ab7462872ab7ce0a61058d2e508
Ben Jenkins <ben@benjenkins.dev>
2023-02-21 11:59:24 -0600

    Revert "chore: remove data-fns dependency"
    This reverts commit b855b1603b6144e8dd3c10a83846e7ce93d85e21.

--- src/components/DateFormatter/index.tsx
+++ src/components/DateFormatter/index.tsx
@@ -1,13 +1,12 @@
+import { parseISO, format } from 'date-fns'
+
 type Props = {
     dateString: string
 }
 
-const options = { dateStyle: 'long' }
 const DateFormatter = ({ dateString }: Props) => {
-    const date = new Date(dateString)
-    // TODO: Add types to match ones expected by data.toLocaleDateString
-    // @ts-expect-error
-    return <time dateTime={dateString}>{date.toLocaleDateString("en-us", options)}</time>
+    const date = parseISO(dateString)
+    return <time dateTime={dateString}>{format(date, 'LLLL	d, yyyy')}</time>
 }
 
 export default DateFormatter
