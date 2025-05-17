c542f9827a8a51d02e3822d09844d8247b52806e
Ben Jenkins <ben@benjenkins.dev>
2025-05-16 22:26:12 -0500

    chore: review todos and make notes more descriptive

--- src/lib/analytics/index.ts
+++ src/lib/analytics/index.ts
@@ -1,16 +1,16 @@
 export const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
 
 export const pageview = (url: string) => {
-  // @ts-expect-error I haven't fixed this yet
+  // @ts-expect-error define window.gtag types
   window.gtag("config", GA_MEASUREMENT_ID, {
     page_path: url,
   });
 };
 
 // TODO: add types here when you use this more
-// @ts-expect-error I haven't fixed this yet
+// @ts-expect-error define function argument types
 export const event = ({ action, category, label, value }) => {
-  // @ts-expect-error I haven't fixed this yet
+  // @ts-expect-error define window.gtag types
   window.gtag("event", action, {
     event_category: category,
     event_label: label,
