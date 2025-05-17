8a6ce0fd5a81cf46f770311d612e8373cfe71df7
Ben Jenkins <ben@benjenkins.dev>
2025-05-16 22:13:49 -0500

    chore: update home page copy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,7 @@ export default function Home() {
   return (
     <>
       <p className="text-xl text-gray-800 dark:text-gray-200">
-        Hey, I&apos;m a Fullstack Software Engineer at Supernatural AI working a distributed marketing platform with exciting AI integrations. I enjoy solving exciting problems and working with
-        a mission-minded team.
+        Hey, I&apos;m a Fullstack Software Engineer at Supernatural AI working on an AI enhanced marketing platform. I love solving unique problems and building software to help people who are putting good out into the world.
       </p>
       <br />
       <p className="text-xl text-gray-800 dark:text-gray-200">
